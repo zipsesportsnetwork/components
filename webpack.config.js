775e44ba786d8c5b4459733de07f48b21dc23588
George <gjp25@zips.uakron.edu>
2020-09-16 00:19:24 -0400

    Build: generate standalone HTML files

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,29 @@
 const { VueLoaderPlugin } = require('vue-loader');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ScriptExtHtmlWebpackPlugin = require('script-ext-html-webpack-plugin');
 
+// A map of overlay name to JS entrypoint. For example, the output file
+// `Debug info.html` is generated from the entrypoint `test-component/index.js`.
+// We use friendlier names to make life slightly easier for our broadcasters.
 const chunks = {
-	'test-component': './test-component/index.js',
-	'team-one-players': './team-one-players/index.js',
-	'team-two-players': './team-two-players/index.js',
+	'Debug info': './test-component/index.js',
+	'Team 1 Players': './team-one-players/index.js',
+	'Team 2 Players': './team-two-players/index.js',
 };
 
 module.exports = {
 	mode: 'development',
 	entry: chunks,
 	output: {
+		// Weird hack to get script-ext-html-webpack-plugin working with
+		// multiple generated HTML files
+		publicPath: '/',
+		// The location where all our output lives
 		path: require('path').resolve(__dirname, 'dist'),
-		filename: '[name]/bundle.js',
+		// What to name each bundled JS file (this is only used intermediately,
+		// the bundle files are eventually inlined into the generated HTML files
+		// and discarded)
+		filename: '[hash].js',
 	},
 	resolve: {
 		extensions: [
@@ -23,10 +34,12 @@ module.exports = {
 	target: 'web',
 	module: {
 		rules: [
+			// vue loader is responsible for turning .vue SFCs into HTML, CSS, and JS chunks
 			{
 				test: /\.vue$/,
 				loader: 'vue-loader',
 			},
+			// plain CSS
 			{
 				test: /\.css$/,
 				use: [
@@ -34,6 +47,7 @@ module.exports = {
 					'css-loader',
 				],
 			},
+			// Sass is compiled to CSS via dart-sass
 			{
 				test: /\.scss$/,
 				use: [
@@ -42,6 +56,7 @@ module.exports = {
 					{ loader: 'sass-loader', options: { implementation: require('dart-sass') } }
 				],
 			},
+			// JS is transpiled with Babel so we can use ESNext fanciness without worry
 			{
 				test: /\.js$/,
 				use: [
@@ -51,11 +66,19 @@ module.exports = {
 		],
 	},
 	plugins: [
+		// Required helper for vue-loader
 		new VueLoaderPlugin(),
+		// Generate an HTML file for each individual component defined at the
+		// top of this file, with a `<script src="">` pointing to the matching
+		// JS bundle
 		...Object.entries(chunks).map(([name, path]) => new HtmlWebpackPlugin({
-			filename: `${name}/index.html`,
+			filename: `${name}.html`,
 			chunks: [name],
 			templateContent: '<div id="app"></div>'
 		})),
+		// Inline all JS files into the HTML files that reference them
+		new ScriptExtHtmlWebpackPlugin({
+			inline: '.js'
+		}),
 	],
 };
